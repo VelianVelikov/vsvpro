34e707c5daaef6b9e941b8da822b9358cafca111
Velian Velikov <vsvelikov@dundee.ac.uk>
2018-11-21 15:29:44 +0000

    Tiles are now fully responsive. Started using unsemantic!

--- js/script.js
+++ js/script.js
@@ -14,8 +14,122 @@ $(document).ready(function () {
   info_box.css('height', width);
   info_box.css('margin-top', -width);
 
+  // i substituted 7px becouse this is the width of the scrollbar
+  if ($(window).width() > 1017){
+    console.log('desktop');
+
+    $ (function () {
+      var biggest = 0;
+      if ($ ('section.tile:nth-child(1) .second-wrapper').height() > biggest){
+        biggest = $ ('section.tile:nth-child(1) .second-wrapper');
+      }
+      if ($ ('section.tile:nth-child(2) .second-wrapper').height() > biggest){
+        biggest = $ ('section.tile:nth-child(2) .second-wrapper');
+      }
+      if ($ ('section.tile:nth-child(3) .second-wrapper').height() > biggest){
+        biggest = $ ('section.tile:nth-child(3) .second-wrapper');
+      }
+
+      $ ('section.tile:nth-child(1) .second-wrapper'). sameBoxHeight ({
+        box: biggest
+      });
+      $ ('section.tile:nth-child(2) .second-wrapper'). sameBoxHeight ({
+        box: biggest
+      });
+      $ ('section.tile:nth-child(3) .second-wrapper'). sameBoxHeight ({
+        box: biggest
+      });
+
+    });
+  }
+  else if ($(window).width() > 760){
+    console.log('tablet');
+
+    $ (function () {
+      var biggest = 0;
+      if ($ ('section.tile:nth-child(1) .second-wrapper').height() > biggest){
+        biggest = $ ('section.tile:nth-child(1) .second-wrapper');
+      }
+      if ($ ('section.tile:nth-child(2) .second-wrapper').height() > biggest){
+        biggest = $ ('section.tile:nth-child(2) .second-wrapper');
+      }
+
+      $ ('section.tile:nth-child(1) .second-wrapper'). sameBoxHeight ({
+        box: biggest
+      });
+      $ ('section.tile:nth-child(2) .second-wrapper'). sameBoxHeight ({
+        box: biggest
+      });
+
+    });
+  }
+  else {
+    console.log('mobile');
+  }
+
+
+
+$(window).resize(function () {
+  $ ('section.tile:nth-child(1) .second-wrapper').css('height', 'auto');
+  $ ('section.tile:nth-child(2) .second-wrapper').css('height', 'auto');
+  $ ('section.tile:nth-child(3) .second-wrapper').css('height', 'auto');
+
+  if ($(window).width() > 1017){
+    console.log('desktop');
+
+    var biggest = 0;
+    if ($ ('section.tile:nth-child(1) .second-wrapper').height() > biggest){
+      biggest = $ ('section.tile:nth-child(1) .second-wrapper');
+    }
+    if ($ ('section.tile:nth-child(2) .second-wrapper').height() > biggest){
+      biggest = $ ('section.tile:nth-child(2) .second-wrapper');
+    }
+    if ($ ('section.tile:nth-child(3) .second-wrapper').height() > biggest){
+      biggest = $ ('section.tile:nth-child(3) .second-wrapper');
+    }
+
+    $ ('section.tile:nth-child(1) .second-wrapper'). sameBoxHeight ({
+      box: biggest
+    });
+    $ ('section.tile:nth-child(2) .second-wrapper'). sameBoxHeight ({
+      box: biggest
+    });
+    $ ('section.tile:nth-child(3) .second-wrapper'). sameBoxHeight ({
+      box: biggest
+    });
+  }
+  else if ($(window).width() > 760){
+
+    console.log('tablet');
+
+    var biggest = 0;
+    var auto = 'auto';
+    if ($ ('section.tile:nth-child(1) .second-wrapper').height() > biggest){
+      biggest = $ ('section.tile:nth-child(1) .second-wrapper');
+    }
+    if ($ ('section.tile:nth-child(2) .second-wrapper').height() > biggest){
+      biggest = $ ('section.tile:nth-child(2) .second-wrapper');
+    }
+
+    $ ('section.tile:nth-child(1) .second-wrapper'). sameBoxHeight ({
+      box: biggest
+    });
+    $ ('section.tile:nth-child(2) .second-wrapper'). sameBoxHeight ({
+      box: biggest
+    });
+    $ ('section.tile:nth-child(2) .second-wrapper'). sameBoxHeight ({
+      box: auto
+    });
+  }
+  else {
+    console.log('mobile');
+  }
+
+  });
 
   $(window).scroll(function () {
+
+
       if ($('header').offset().top == 0){
         $('.arrow').show();
       }
@@ -23,6 +137,7 @@ $(document).ready(function () {
         $('.arrow').css('display', 'none');
       }
   });
+
 
 
   //                Start back-to-top button jQuery
